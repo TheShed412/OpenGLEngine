2a63c73c0860a17e55ffa01e579581a832e32787
Tyler Bobik <shed412@gmail.com>
2019-02-02 22:06:32 -0500

    added tans init shader

---
+++ shaders/vshader.glsl
@@ -0,0 +1,18 @@
+#version 120
+
+attribute vec4 vPosition;
+attribute vec4 vColor;
+attribute vec2 vTexCoord;
+varying vec2 texCoord;
+varying vec4 color;
+
+uniform mat4 ctm;
+uniform mat4 projection;
+uniform mat4 model_view;
+
+void main()
+{
+	color = vColor;
+	gl_Position = projection * model_view * ctm * vPosition / vPosition.w;
+	texCoord = vTexCoord;
+}
