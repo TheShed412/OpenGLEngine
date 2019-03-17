8626542aa5f73f17a1c53f163a09f2c8d89483ec
Tyler Bobik <shed412@gmail.com>
2019-03-17 00:56:06 -0400

    need to check for another case when parsing the face indexes

--- lib/ObjectLoader.cpp
+++ lib/ObjectLoader.cpp
@@ -7,7 +7,7 @@
 ObjectLoader::ObjectLoader() {}
 
 bool ObjectLoader::LoadObject(const char* path, vector<vec3>& out_verts, vector<vec2>& out_uvs, vector<vec3>& out_norms) {
-    vector<unsigned int> vector_indecies, uv_indices, noramal_indecies;
+    vector<int> vector_indecies, uv_indices, noramal_indecies;
     vector<vec3> temp_verts;
     vector<vec2> temp_uvs;
     vector<vec3> temp_norms;
@@ -63,15 +63,18 @@ bool ObjectLoader::LoadObject(const char* path, vector<vec3>& out_verts, vector<
             string face3;
             modelLineStream >> face1 >> face2 >> face3;
 
-            unsigned int vertIndex[3], uvIndex[3], normalIndex[3];
-            sscanf(face1.c_str(), "%d/%d/%d", &vertIndex[0], &uvIndex[0], &normalIndex[0]);
+            // I need to check for the case that the texture vertex doesn't exit. poo
+            int vertIndex[3], uvIndex[3], normalIndex[3];
+            int filled = sscanf(face1.c_str(), "%d/%d/%d", &vertIndex[0], &uvIndex[0], &normalIndex[0]);
             sscanf(face2.c_str(), "%d/%d/%d", &vertIndex[1], &uvIndex[1], &normalIndex[1]);
             sscanf(face3.c_str(), "%d/%d/%d", &vertIndex[2], &uvIndex[2], &normalIndex[2]);
             for(int i = 0; i < 3; i++) {
                 vector_indecies.push_back(vertIndex[i]);
-                uv_indices.push_back(uvIndex[i]);
                 noramal_indecies.push_back(normalIndex[i]);
+                uv_indices.push_back(uvIndex[i]);
             }
+            cout << "filled " << filled << endl;
+            cout << "normalIndex " << normalIndex[0] << endl;
         }
     }
 
