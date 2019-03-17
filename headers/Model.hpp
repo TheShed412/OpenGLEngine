c905a8cb16a9eab69b6db9086804e31219ddd9e1
Tyler Bobik <shed412@gmail.com>
2019-03-16 21:47:47 -0400

    not using this version of model

--- headers/Model.hpp
+++ headers/Model.hpp
@@ -1,33 +1,42 @@
 #ifndef MODEL_THING_SHIT
 #define MODEL_THING_SHIT
 
-#include <string>
-#include <vector>
+// #include <string>
+// #include <fstream>
+// #include <sstream>
+// #include <iostream>
+// #include <map>
+// #include <vector>
 
-#include "Mesh.hpp"
-#include "Shader.hpp"
+// #include "../include/assimp/Importer.hpp"
+// #include "../include/assimp/scene.h"
+// #include "../include/assimp/postprocess.h"
+// #include "stb_image.h"
+// #include "Mesh.hpp"
+// #include "Shader.hpp"
 
-using namespace std;
+// using namespace std;
 
-class Model 
-{
-    public:
-        /*  Functions   */
-        Model(char *path)
-        {
-            loadModel(path);
-        }
-        void Draw(Shader shader);	
-    private:
-        /*  Model Data  */
-        vector<Mesh> meshes;
-        string directory;
-        /*  Functions   */
-        void loadModel(string path);
-        // void processNode(aiNode *node, const aiScene *scene);
-        // Mesh processMesh(aiMesh *mesh, const aiScene *scene);
-        // vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, 
-        //                                      string typeName);
-};
+// class Model 
+// {
+//     public:
+//         /*  Functions   */
+//         Model(char *path)
+//         {
+//             loadModel(path);
+//         }
+//         void Draw(Shader shader);	
+//     private:
+//         /*  Model Data  */
+//         vector<Texture> textures_loaded; 
+//         vector<Mesh> meshes;
+//         string directory;
+//         /*  Functions   */
+//         void loadModel(string path);
+//         void processNode(aiNode *node, const aiScene *scene);
+//         Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+//         vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, 
+//                                              string typeName);
+// };
 
 #endif
