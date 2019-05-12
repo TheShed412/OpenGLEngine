ec09b0dec0031c6d86886e3f1f0fccab7f57e97c
Tyler Bobik <shed412@gmail.com>
2019-05-12 01:34:53 -0400

    processing seems to work

--- lib/ObjectLoader.cpp
+++ lib/ObjectLoader.cpp
@@ -76,12 +76,31 @@ bool ObjectLoader::LoadObject(const char* path, vector<vec3>& out_verts, vector<
                     vector_indecies.push_back(vertIndex[i]);
                     noramal_indecies.push_back(normalIndex[i]);
                 }
-                cout << "no uv" << endl;
-                cout << "normalIndex " << normalIndex[0] << endl;
             }
         }
     }
 
+    /*
+        Processing step. Take the f part and use it to construct our outputs
+    */
 
-    return false;
+   for(int i = 0; i < vector_indecies.size(); i++) {
+       int vector_index = vector_indecies[i];
+       vec3 vector = temp_verts[vector_index-1];
+       out_verts.push_back(vector);
+   }
+
+   for(int i = 0; i < uv_indices.size(); i++) {
+       int uv_index = uv_indices[i];
+       vec2 uv = temp_uvs[uv_index-1];
+       out_uvs.push_back(uv);
+   }
+
+   for(int i = 0; i < noramal_indecies.size(); i++) {
+       int norm_index = noramal_indecies[i];
+       vec3 norm = temp_norms[norm_index-1];
+       out_norms.push_back(norm);
+   }
+
+    return true;
 }
